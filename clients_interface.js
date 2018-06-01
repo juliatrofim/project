e95345a12cd43ff5fb11f17fe6aaa73ff82acb8e
juliatrofim <djuliya.trofimova@gmail.com>
2018-06-01 21:43:09 +0300

    add readme

--- clients_interface.js
+++ clients_interface.js
@@ -22,22 +22,22 @@ $(document).ready(function(){
         document.getElementById("likebutton").childNodes[0].nodeValue = text1;
         document.getElementById("dislikebutton").childNodes[0].nodeValue = text2;
         if (data.Button === "1") {
-            document.getElementById("like").id = "like1";
+            document.getElementById("like").id = "like2";
         }
         if (data.Button === "1") {
-            document.getElementById("dislike").id = "dislike1";
+            document.getElementById("dislike").id = "dislike2";
         }
         if (data.Button === "2") {
-            document.getElementById("like").id = "like1";
-        }
-        if (data.Button === "2") {
-            document.getElementById("dislike").id = "dislike1";
-        }
-        if (data.Button === "3") {
             document.getElementById("like").id = "like3";
         }
-        if (data.Button === "3") {
+        if (data.Button === "2") {
             document.getElementById("dislike").id = "dislike3";
+        }
+        if (data.Button === "3") {
+            document.getElementById("like").id = "like1";
+        }
+        if (data.Button === "3") {
+            document.getElementById("dislike").id = "dislike1";
         }
     });
     $("#content video source").attr("src", name);
